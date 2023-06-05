0f97da3a7d7a65d13d9def94ee9bb2b068cc3a7d
Emma Gerigscott <elgerig@gmail.com>
2023-06-04 20:40:45 -0700

    add database and seed data

---
+++ CretaceousApi/Migrations/20230605034009_SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CretaceousApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Scorpo", "Woolly Mammoth" },
+                    { 2, 10, "DangleDoo", "Dinosaur" },
+                    { 3, 2, "Flexorina", "Dinosaur" },
+                    { 4, 4, "Zibzab", "Shark" },
+                    { 5, 22, "BigFrank", "Dinosaur" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
